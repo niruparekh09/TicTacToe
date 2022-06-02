86a9e75caf57aaf9c6d64f6396c69784cd4fd5b5
Nirav Parekh <niruparekh09@gmail.com>
2022-06-02 17:26:24 +0530

    git init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+    </>
+  );
+}
+
+export default App;
